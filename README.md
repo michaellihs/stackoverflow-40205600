cd20764120b136b6cff3dd4da38da62105a588e4
Michael Lihs (AA/PJ-COSA) <michael.lihs@de.bosch.com>
2016-10-23 23:19:25 +0200

    [TASK] Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Spring Boot Sample Application
+==============================
+
+This is a test for the [StackOverflow Post 40205600](http://stackoverflow.com/questions/40205600/spring-boot-app-terminating-at-startup)
+
+Start the application with
+
+    mvn spring-boot:run
---
+++ src/main/java/com/example/RestBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBootApplication.class, args);
+    }
+
+}
